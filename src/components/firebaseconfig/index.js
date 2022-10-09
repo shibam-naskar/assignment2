19a2642ab26aaa95278e788edb1d2ede8403736b
shibam-naskar <shibamnskar@gmail.com>
2022-10-09 16:26:07 +0530

    new

--- src/components/firebaseconfig/index.js
+++ src/components/firebaseconfig/index.js
@@ -3,7 +3,12 @@ import {getDatabase} from 'firebase/database';
 
 function StartFirebase() {
     const firebaseConfig = {
-        
+        apiKey: "AIzaSyBSfcLGxZ0ODlq1H5YTpAzQJwMvQ3i1sxg",
+        authDomain: "assignment2-21a5d.firebaseapp.com",
+        projectId: "assignment2-21a5d",
+        storageBucket: "assignment2-21a5d.appspot.com",
+        messagingSenderId: "508251823697",
+        appId: "1:508251823697:web:365d946cf770a621d92270"
       };
       
       // Initialize Firebase
